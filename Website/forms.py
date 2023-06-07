b9d2be318c27daaf627f1d368d92cc79d4d0ab61
Anish <86150580+savysanish56@users.noreply.github.com>
2023-06-07 22:44:57 +0930

    bug fixes on registration and pop-up setup

--- Website/forms.py
+++ Website/forms.py
@@ -5,11 +5,11 @@ from django import forms
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label = "", widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Email Address'}))
     first_name = forms.CharField(label = "", max_length = 100, widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'First Name'}))
-    last_name = forms.CharField(label = "", max_length = 100, widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(label = "", max_length = 100, widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Last Name'}))
 
     class Meta:
         model = User
-        fileds = ('username', 'first_name', 'last_name', 'email', 'password1','password2')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1','password2')
 
 
     def __init__(self, *args, **kwargs):
