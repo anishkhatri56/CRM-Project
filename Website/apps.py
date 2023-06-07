830da6cc3339c6ff2b7ad47687bbb0b42bd8409b
Anish <86150580+savysanish56@users.noreply.github.com>
2023-06-07 21:31:02 +0930

    Login and Logout funcationalities

--- Website/apps.py
+++ Website/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class WebstieConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Webstie'
+    name = 'Website'
---
+++ Website/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect 
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib import messages
+
+
+def home(request):
+    #for user logging check 
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username = username, password = password )
+        if user is not None: 
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error in Logging Details, Please try again")
+            return redirect('home')
+    else:
+       return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have Been logged out")
+    return redirect('home')
