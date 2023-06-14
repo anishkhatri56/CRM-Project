e135a53487ed3b778881e9b225fb620d4be62bed
Anish <86150580+savysanish56@users.noreply.github.com>
2023-06-14 23:21:54 +0930

    CRUD functionalities

--- Website/views.py
+++ Website/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all()
+
     #for user logging check 
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,7 +22,7 @@ def home(request):
             messages.success(request, "There was an error in Logging Details, Please try again")
             return redirect('home')
     else:
-       return render(request, 'home.html', {})
+       return render(request, 'home.html', {'records':records})
 
 
 def logout_user(request):
@@ -47,3 +50,26 @@ def register_user(request):
         return render(request, 'register.html', {'form':form} )
         
     return render(request, 'register.html',{'form':form} )
+
+
+def customer_record(request,pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id = pk)
+        return render(request, 'record.html',{'customer_record':customer_record} )
+    else:
+        messages.success(request, "You must be Logged In to acess this page.....")
+        return redirect('home')
+    
+def delete_record(request,pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id = pk) 
+        delete_it.delete()
+        messages.success(request, "Record Deleted Sucessfully.....")
+        return redirect('home')
+    else: 
+        messages.success(request, "Please create account to prompt this action.....")
+        return redirect('home')
+
+
+def add_record(request):
+    return render (request, 'add_record.html', {})
