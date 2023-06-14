38fe12f38ca6a43daad2d5aae63a4827a6ba529f
Anish <86150580+savysanish56@users.noreply.github.com>
2023-06-15 00:11:24 +0930

    deployment, txt files

--- CRM/settings.py
+++ CRM/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-6xh^xhmp=6xhaz7cx^s&8fd7qb6zc_2ha!39(x+fw8zr5ed@@0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-crm-anish.herokuapp.com']
 
 
 # Application definition
@@ -123,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+if os.getcwd() == '/app':
+    DEBUG = False
